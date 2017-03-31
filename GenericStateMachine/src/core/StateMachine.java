5829dd1c96b84726a799aa8883bf68c1c06bebf5
ThramboulidisKleanthis <thrambo@ece.upatras.gr>
2017-03-31 18:32:07 +0300

    2nd draft layout that hopefully captures the main behavior of execute

--- GenericStateMachine/src/core/StateMachine.java
+++ GenericStateMachine/src/core/StateMachine.java
@@ -3,9 +3,11 @@ package core;
 public class StateMachine {
 	MessageQueue itsMsgQ;
 	Event curEvent;
-	
-	public void execute(){
-		while(curState != FINAL_STATE)		// FINAL_STATE is the state machine's final state
+	State curState;
+	Transition activeTransition;
+		
+public void execute(){
+	while(curState != FINAL_STATE){		// FINAL_STATE is the state machine's final state
 		curEvent = itsMsgQ.getNext();
 		if(activeTrasnition =(curState.getActiveTransition(curEvent))==null){
 			if(curState.defEventPool.isDeffered(curEvent)){
@@ -16,14 +18,24 @@ public class StateMachine {
 			}
 		} 
 		else{	// fire transition
+			curState.exit();
+			//curState.doActivity.terminate();		// to be defined later
 			activeTrasnition.effect();
 			curState=activeTrasnition.itsTargetState;
-			curState.entry();
-			checkDeferredEvents();		//  returns a valid for the state deffered event for further processing
-			do();	// to be defined later
+			if(curState!=FINAL_STATE){
+				curState.entry();
+				if(getValidDeferredEvent()!=null){ // gets a valid for the current state event from the deferred Pool, if any 
+					//  returns a valid for the current state deferred event for further processing
+				}
+				do();	// to be defined later
+				if(curState.hasCompletionTransition()){		// a Completion transition is one without a trigger
+					// proceed to fire the transition 
+				}
+				
+			}
 			
 		}
-			
+	}		
 		
-	}
+}
 }
