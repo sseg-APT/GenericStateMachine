3a59354d54f2191388ad663092dca3b6dcd1043a
John Buluba <buluba89@gmail.com>
2017-10-12 17:30:07 +0300

    Javadoc

--- GenericStateMachine/src/main/java/uml4iot/GenericStateMachine/core/MessageQueue.java
+++ GenericStateMachine/src/main/java/uml4iot/GenericStateMachine/core/MessageQueue.java
@@ -6,10 +6,12 @@ import java.util.Stack;
 import java.util.concurrent.LinkedBlockingDeque;
 
 
+/**
+ * The message queue for the state machine. Its a LinkedBlockingDeque that
+ * takes SMReception objects and some extra functionality
+ */
 public class MessageQueue extends LinkedBlockingDeque<SMReception> {
-	/**
-	 * 
-	 */
+
 	private static final long serialVersionUID = 1L;
 	private ArrayList<MessageQueue> childList;
 
@@ -17,13 +19,23 @@ public class MessageQueue extends LinkedBlockingDeque<SMReception> {
 		childList = new ArrayList<>(10);
 	}
 
+	/**
+	 * Add a new object object in the queue
+	 * {@link java.util.concurrent.LinkedBlockingDeque#add(Object)}
+	 * @param e the new object to be added in the queue
+	 * @return return true if insert is successful
+	 */
 	@Override
 	public boolean add(SMReception e) {
 		forwardToChilds(e);
 		return super.add(e);
 	}
 
-
+	/**
+	 * Gets the next item in the queue. Blocking method
+	 * @return the next item
+	 * @throws InterruptedException when interrupted if blocked
+	 */
 	public SMReception getNext() throws InterruptedException{
 		// TODO Auto-generated method stub
 		SMReception ev=null;
@@ -31,6 +43,13 @@ public class MessageQueue extends LinkedBlockingDeque<SMReception> {
 		return ev;
 	}
 
+	/**
+	 * Gets the next item in the queue, except if it is in the ignoreSet.
+	 * Like {@link #add(SMReception)} but takes an extra argument, the ignoreSet.
+	 * @param ignoreSet A set of events that we don't want
+	 * @return the next item
+	 * @throws InterruptedException when interrupted if blocked
+	 */
 	public SMReception getNext(Collection<SMReception> ignoreSet) throws InterruptedException{
 		Stack<SMReception> ignored = new Stack<>();
 		SMReception ev = null;
@@ -49,14 +68,30 @@ public class MessageQueue extends LinkedBlockingDeque<SMReception> {
 		return ev;
 	}
 
+	/**
+	 * Adds a child queue to copy and forward all objects that are added in this queue
+	 * Useful when state machine branches
+	 * @param queue A queue to be added as child
+	 */
 	public void addChildQueue(MessageQueue queue){
 		childList.add(queue);
 	}
 
+
+	/**
+	 * Removes a child queue that was previously added. Useful when the state machine branches
+	 * merge
+	 * @param queue the child queue to remove
+	 */
 	public void removeChildQueue(MessageQueue queue){
 		childList.remove(queue);
 	}
 
+
+	/**
+	 * Forwards a message to all child queues
+	 * @param message the message to be forwarded
+	 */
 	private void forwardToChilds(SMReception message){
 		for (MessageQueue child : childList) {
 			child.add(message);
--- GenericStateMachine/src/main/java/uml4iot/GenericStateMachine/core/State.java
+++ GenericStateMachine/src/main/java/uml4iot/GenericStateMachine/core/State.java
@@ -6,12 +6,28 @@ import java.util.HashSet;
 import java.util.List;
 
 
+/**
+ *This class defines a single state in a state machine
+ * and its operations
+ */
 public abstract class State {
 	private List<Transition> ourTrans;
 	private Collection<SMReception> ourDefEvents;
 
+	/**
+	 * This method is called when the state machine is entering this state
+	 */
 	abstract protected void entry();
+
+	/**
+	 * This method is called after the entry state. Use this method to implement
+	 * the main work of a state
+	 */
 	abstract protected void doActivity();
+
+	/**
+	 * This method is called when the state machine is exits this state
+	 */
 	abstract protected void exit();
 
 	public State(){
@@ -19,10 +35,20 @@ public abstract class State {
 		this.ourDefEvents = new HashSet<>();
 	}
 
+	/**
+	 * Use this method to add a transition to this state
+	 * @param t: The transition to be added
+	 */
 	public void addTransition(Transition t) {
 		ourTrans.add(t);
 	}
 
+
+	/**
+	 * This method check if there is a transition that is on completion, meaning that
+	 * it does not need an event to trigger
+	 * @return True if there is a completion transition, false if there is not
+	 */
 	protected boolean hasCompletionTrans(){
 		for (Transition trans : ourTrans) {
 			if (trans.isCompletion()){
@@ -32,15 +58,28 @@ public abstract class State {
 		return false;
 	}
 
+	/**
+	 * Add deferred events for this transition. Deferred events are events that are ignored for
+	 * this state but don't get discarded from the queue
+	 * @param event the event to be added to the deferred event list
+	 */
 	public void addDeferredEvent(SMReception event){
 		this.ourDefEvents.add(event);
 	}
 
+	/**
+	 * Return a list with all the deferred events for this state
+	 * @return A collection with events
+	 */
 	public Collection<SMReception> getDeferredEvents(){
 		return ourDefEvents;
 	}
 
-
+	/**
+	 * Checks if a transition is triggered
+	 * @param curEvent the event to check if triggers a transition
+	 * @return A transition that is triggered by the curEvent, null if no transition is triggered
+	 */
 	public Transition getActiveTransition(SMReception curEvent) {
 		//If null check for completion transition
 		if (curEvent == null) {
@@ -55,6 +94,10 @@ public abstract class State {
 		return null;
 	}
 
+	/**
+	 * Get a completion transition if there is one
+	 * @return A transition that is on completion if there is one, null if not
+	 */
 	private Transition getCompletionTransition(){
 		for (Transition trans : ourTrans) {
 			if (trans.isCompletion()){
