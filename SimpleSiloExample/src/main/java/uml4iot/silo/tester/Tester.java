ac7c589417ee88c2c8f286cfb3040ef0873b6e44
John Buluba <buluba89@gmail.com>
2018-10-24 14:29:20 +0300

    Added SimpleSiloExample

---
+++ SimpleSiloExample/src/main/java/uml4iot/silo/tester/Tester.java
@@ -0,0 +1,17 @@
+package uml4iot.silo.tester;
+
+public class Tester {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		SimpleSiloCtr ss = new SimpleSiloCtr();
+		SignalGenerator sg = new SignalGenerator(ss.itsMsgQ);
+		new Thread(sg).start();
+		Thread smt=new Thread(ss);
+		smt.setName("mainSmT");
+		smt.start();
+		smt.join();
+		System.out.println("end");
+	}
+
+}
